c8cabf1570fe3045f6e0e418f26820fe0290d5e0
Asus <Asus@LAPTOP-CPBK65PL>
2021-08-05 15:16:57 +0500

    Add project files.

---
+++ WordAddIn1/Ribbon1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Interop.Word;
+
+namespace WordAddIn1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, RibbonControlEventArgs e)
+        {
+
+           
+            string url = "http://tahrirchi.net/api/corrections/";
+            using (var webClient = new WebClient())
+            {
+                string title = "Title";
+                var pars = new NameValueCollection();
+                pars.Add("text", "bolaa");
+                var response = webClient.UploadValues(url, pars);
+                string str = System.Text.Encoding.UTF8.GetString(response);
+                MessageBox.Show(str, title);
+
+                Console.WriteLine(str);
+                Console.ReadKey();
+            }
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            string message = "Чтения";
+            string title = "Title";
+            var word = new Microsoft.Office.Interop.Word.Application();
+            var doc = word.ActiveDocument;
+            Microsoft.Office.Interop.Word.Paragraphs paras = doc.Paragraphs;
+            
+           // application.System.Cursor = wdCursorWait;
+
+            MessageBox.Show(doc.Paragraphs[1].Range.Text, title);
+            
+        }
+    }
+}
---
+++ WordAddIn1/ThisAddIn.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace WordAddIn1
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.DocumentBeforeSave += new Word.ApplicationEvents4_DocumentBeforeSaveEventHandler(Application_DocumentBeforeSave);
+            this.Application.DocumentOpen += new Word.ApplicationEvents4_DocumentOpenEventHandler(WorkWithDocument);
+
+            ((Word.ApplicationEvents4_Event)this.Application).NewDocument +=
+                new Word.ApplicationEvents4_NewDocumentEventHandler(WorkWithDocument);
+
+
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        void Application_DocumentBeforeSave(Word.Document Doc, ref bool SaveAsUI, ref bool Cancel)
+        {
+            Word.Paragraphs paras = Doc.Paragraphs;
+
+            for (int i = 0; i < paras.Count; i++)
+            {
+                //paras[i].Range.;
+            }
+            string message = "Чтения";
+            string title = "Title";
+            System.Windows.Forms.MessageBox.Show(Doc.Paragraphs[1].Range.Text, title);
+            Doc.Paragraphs[1].Range.InsertParagraphBefore();
+            Doc.Paragraphs[1].Range.Text = "This text was added by using code.";
+        }
+
+        #endregion
+        private void WorkWithDocument(Microsoft.Office.Interop.Word.Document Doc)
+        {
+            try
+            {
+                Word.Range rng = Doc.Range(0, 0);
+                rng.Text = "New Text";
+                rng.Select();
+            }
+            catch (Exception ex)
+            {
+                // Handle exception if for some reason the document is not available.
+            }
+           
+
+        }
+    }
+}
